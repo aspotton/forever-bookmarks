4b9c4595c6842bf20348525309b1f4221b261f61
Adam Spotton <aspotton@gmail.com>
2022-10-25 21:08:50 -0400

    Added better error page/site down styling

--- html/js/unavailable.js
+++ html/js/unavailable.js
@@ -3,4 +3,10 @@ var url_params = new URLSearchParams(query_string);
 var original_url = url_params.get('url');
 
 document.getElementById('URL').innerHTML = original_url;
-location.href = 'https://web.archive.org/web/2/' + original_url;
+document.getElementById('IAButton').addEventListener('click', checkIA);
+
+function checkIA() {
+	document.body.style.cursor = 'wait';
+	document.getElementById('IAButton').disabled = true;
+	location.href = 'https://web.archive.org/web/2/' + original_url;
+}
